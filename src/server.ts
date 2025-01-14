25683c9451f610ea5fd9554c52d1001580b94c59
Thiago Vasconcelos <122023431+thiagovasconcelosmiranda@users.noreply.github.com>
2025-01-14 12:36:55 -0300

    N

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const port = process.env.PORT ?? 4000;
 app.use(fileUpload());
 app.use(cors());
 app.use(express.urlencoded({extended: false}));
-app.use(express.static('public/'));
+app.use(express.static('public'));
 app.use(router);
 
 app.listen(port, () => console.log(`Server http://localhost:${port}`));
