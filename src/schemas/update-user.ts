9b6a289a9394a910c287a217c42a2bfd42773f0e
Thiago Vasconcelos <122023431+thiagovasconcelosmiranda@users.noreply.github.com>
2025-02-06 15:11:35 -0300

    Nova atualização

--- src/schemas/update-user.ts
+++ src/schemas/update-user.ts
@@ -2,6 +2,6 @@ import { z } from "zod";
 
 export const updateUserSchema = z.object({
     name: z.string().min(2, 'Precisa ter 2 ou mais caracteres').optional(),
-    bio: z.string().optional(),
-    link: z.string().url('Precisa ser uma url valida').optional()
+    bio: z.string().min(2, 'Precisa ter 2 ou mais caracteres').optional(),
+    link: z.string().min(2,'Precisa ser uma url valida').optional()
 });
